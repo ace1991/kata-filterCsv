95d8530b7564baa73b5519a7c2dc6cb8b89ca909
acaleza <adrian.calvo1@gmail.com>
2023-05-01 11:20:38 +0200

    green: Csv Filter excludes lines with both tax fields are exclusive

--- src/core/filter-csv.ts
+++ src/core/filter-csv.ts
@@ -1,9 +1,15 @@
 export class CsvFilter {
-    constructor(private readonly lines: string[]) {}
+    private constructor(private readonly lines: string[]) {}
     static create(lines: string[]) {
         return new CsvFilter(lines);
     }
     get filteredLines() {
-        return this.lines;
+        const result = [];
+        result.push(this.lines[0]);
+        const fields = this.lines[1].split(",");
+        if (!fields[4] || !fields[5]) {
+            result.push(this.lines[1]);
+        }
+        return result;
     }
 }
--- src/tests/filter-csv.test.ts
+++ src/tests/filter-csv.test.ts
@@ -1,7 +1,7 @@
 import {CsvFilter} from "../core/filter-csv";
 
 describe('CSV Filter', () => {
-    it('Allow only correct lines', () => {
+    it('allow only correct lines', () => {
         const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
         const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
         const csvFilter = CsvFilter.create([header, invoiceLine]);
