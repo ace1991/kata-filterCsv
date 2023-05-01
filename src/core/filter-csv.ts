76bd4b0c6d06961fbfae8a3042112f5b954c67ed
acaleza <adrian.calvo1@gmail.com>
2023-05-01 11:28:00 +0200

    refactor: Csv Filter excludes lines with both tax fields are exclusive

--- src/core/filter-csv.ts
+++ src/core/filter-csv.ts
@@ -1,4 +1,6 @@
 export class CsvFilter {
+
+    private readonly _separator = ",";
     private constructor(private readonly lines: string[]) {}
     static create(lines: string[]) {
         return new CsvFilter(lines);
@@ -6,10 +8,18 @@ export class CsvFilter {
     get filteredLines() {
         const result = [];
         result.push(this.lines[0]);
-        const fields = this.lines[1].split(",");
-        if (!fields[4] || !fields[5]) {
+        const fields = this.splitFlieds();
+        if (this.isNotFilledIvaAndIgic(fields)) {
             result.push(this.lines[1]);
         }
         return result;
+    }
+
+    private isNotFilledIvaAndIgic(fields: string[]) {
+        return !fields[4] || !fields[5];
+    }
+
+    private splitFlieds() {
+        return this.lines[1].split(this._separator);
     }
 }
